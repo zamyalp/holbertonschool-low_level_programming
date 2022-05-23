3e858aca3f5f3520c8ac0b1d34a7116329d6c439
zamyalp <zamyalp03@gmail.com>
2022-05-23 13:36:53 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+char alpha = 'a';
+
+while (alpha <= 'z')
+{
+
+putchar(alpha);
+alpha++;
+
+}
+putchar('\n');
+
+return 0;
+
+}
