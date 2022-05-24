300571748e605e7e32aa6230d2dc759892c8d76a
zamyalp <zamyalp03@gmail.com>
2022-05-24 15:30:39 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,6 +3,10 @@
 /**
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  */
+/**
+ *print_alphabet - prints a string ina a array via a loop
+ */
+
 void print_alphabet(void)
 {
 char ch;
