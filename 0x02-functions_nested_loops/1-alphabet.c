56fdb41fc8e3b2aea7993acb8bb4c3e78321f77e
zamyalp <zamyalp03@gmail.com>
2022-05-24 14:44:29 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,15 +5,14 @@
  */
 void print_alphabet(void)
 {
-	char ch;
+char ch;
+ch = 'a';
 
-	ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
 
-	while (ch <= 'z')
-	{
-		_putchar(ch);
-		ch++;
-	}
-
-	_putchar('\n');
+_putchar('\n');
 }
