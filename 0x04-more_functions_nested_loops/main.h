5714fc1090979d88fcc94574f1d98ea95362858a
zamyalp <zamyalp03@gmail.com>
2022-05-30 12:35:33 -0700

    6

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,9 +11,13 @@ int _putchar(char c);
 void more_numbers(void);
 int _putchar(char c);
 void print_line(int n);
+int _putchar(char c);
 void print_diagonal(int n);
+int _putchar(char c);
 void print_square(int size);
+int _putchar(char c);
 void print_triangle(int size);
+int _putchar(char c);
 void print_number(int n);
 
 #endif
