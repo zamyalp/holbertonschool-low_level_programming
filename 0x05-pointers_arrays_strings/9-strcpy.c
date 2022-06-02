cf24bac1f8bb7368d85995547079cc56f926a17c
zamyalp <zamyalp03@gmail.com>
2022-06-02 12:43:31 -0700

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,11 +9,20 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int len, i;
 
-	for (; src[i] != '\0'; i++)
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
 	}
+	dest[i] = '\0';
+
 	return (dest);
 }
