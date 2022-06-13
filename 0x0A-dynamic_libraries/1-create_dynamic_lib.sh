1a8b7c49a5d559ff29dd81be8ce3ae5ff734a16c
zamyalp <zamyalp03@gmail.com>
2022-06-13 14:59:59 -0700

    1-

--- 0x0A-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x0A-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -fPIC -c ./*.c
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -shared -o libdynamic.so *.o
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -shared -o liball.so *.o
