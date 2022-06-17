af88d5432bfd4809e32ae95ac558093f7a0c4744
zamyalp <zamyalp03@gmail.com>
2022-06-16 20:52:07 -0700

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
+ *_strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ * @str: string to duplicate
+ * Return: pointer to duplicated string in allocated memory
  */
+
 char *_strdup(char *str)
 {
-	int i, end;
-	char *array;
+	char *duplicate_str;
+	int i = 0, len = 0;
 
-	if (str == NULL)
+	if (str == NULL) /* validate str input */
 		return (NULL);
 
-	for (end = 0; end <= *str; end++)
+	while (*(str + i))
+		len++, i++;
+	len++; /* add null terminator to length */
+
+	duplicate_str = malloc(sizeof(char) * len); /* allocate memory */
+
+	if (duplicate_str == NULL) /* validate memory */
+		return (NULL);
+
+	i = 0;
+	while (i < len)
 	{
+		*(duplicate_str + i) = *(str + i);
+		i++;
 	}
 
-	end += 1;
-	array = malloc(sizeof(char) * end);
-
-	for (i = 0; i < end; i++)
-		array[i] = str[i];
-
-	if (array == NULL)
-		return (NULL);
-	return (array);
+	return (duplicate_str);
 }
