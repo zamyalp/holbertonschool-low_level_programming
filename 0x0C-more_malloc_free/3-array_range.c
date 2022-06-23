2a00dce8515d6277896b8302d4e39b20b712b354
zamyalp <zamyalp03@gmail.com>
2022-06-23 13:15:42 -0700

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,21 +10,21 @@
 
 int *array_range(int min, int max)
 {
-  int *ptr;
-  int i;
-  int n = (max - min + 1);
+	int *ptr;
+	int i;
+	int n = (max - min + 1);
 
-  if (min > max) /* validate input */
-    return (NULL);
+	if (min > max) /* validate input */
+		return (NULL);
 
-  /* malloc and check for error */
-  ptr = malloc(sizeof(int) * n);
-  if (ptr == NULL)
-    return (NULL);
+	/* malloc and check for error */
+	ptr = malloc(sizeof(int) * n);
+	if (ptr == NULL)
+		return (NULL);
 
-  /* set values */
-  for (i = 0; i < n; i++)
-    ptr[i] = min++;
+	/* set values */
+	for (i = 0; i < n; i++)
+		ptr[i] = min++;
 
-  return (ptr);
+	return (ptr);
 }
