70d9e33382a6a7ad3bd5aa19fa45ce43662c6864
zamyalp <zamyalp03@gmail.com>
2022-06-28 17:41:48 -0700

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,34 +1,50 @@
-#include <stdarg.h> /* va_* */
-#include <stdio.h> /* printf */
+#include <stdarg.h>
+#include <stdio.h>
 #include "variadic_functions.h"
-
 /**
- * print_strings - print varying amount of strings
- * @separator: delimiter
- * @n: amount of arguments in list
- */
-
-void print_strings(const char *separator, const unsigned int n, ...)
+* print_all - Entry Point
+* c = char, i = int, f = float, s = char * (if null print (nil))
+* @format: list of arg types
+* Return: 0
+*/
+void print_all(const char * const format, ...)
 {
 	va_list valist;
-	unsigned int i;
-	char *s;
+	int n = 0, i = 0;
+	char *sep = ", ";
+	char *str;
 
-	if (n > 0)
+	va_start(valist, format);
+
+	while (format && format[i])
+		i++;
+
+	while (format && format[n])
+{
+	if (n  == (i - 1))
 	{
-		va_start(valist, n);
-		for (i = 1; i <= n; i++)
-		{
-			s = va_arg(valist, char *);
-			if (s == NULL)
-				printf("(nil)");
-			else
-				printf("%s", s);
-
-			if (i != n && separator != NULL)
-				printf("%s", separator);
-		}
-		va_end(valist);
+		sep = "";
 	}
+	switch (format[n])
+	{
+	case 'c':
+		printf("%c%s", va_arg(valist, int), sep);
+		break;
+	case 'i':
+		printf("%d%s", va_arg(valist, int), sep);
+		break;
+	case 'f':
+		printf("%f%s", va_arg(valist, double), sep);
+		break;
+	case 's':
+		str = va_arg(valist, char *);
+		if (str == NULL)
+			str = "(nil)";
+		printf("%s%s", str, sep);
+		break;
+	}
+	n++;
+}
 	printf("\n");
+	va_end(valist);
 }
