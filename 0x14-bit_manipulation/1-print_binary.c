19dd49c977536aecd90db4e85209d24aae70f113
zamyalp <zamyalp03@gmail.com>
2022-07-13 15:51:34 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,28 +7,42 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i = 0, count, k, temp;
 
-	if (n == 0)
+	unsigned long int n_copy = n, mask = 1;
+	int len = 0;
+
+	while (n_copy > 0)
 	{
-		printf("0");
-		return;
+		len++;
+		n_copy >>= 1;
 	}
+	len -= 1;
 
-	temp = n;
+	if (len > 0) /* create mask based on length of num */
+		mask = mask << len;
 
-	while (temp != 0)
+	while (mask > 0) /* match each rightmost bit to see if 1 or 0 */
 	{
-		i++;
-		temp = temp >> 1;
-	}
-
-	for (count = i - 1; count >= 0; count--)
-	{
-		k = n >> count;
-		if (k & 1)
-			printf("1");
+		if (n & mask)
+			_putchar('1');
 		else
-			printf("0");
+			_putchar('0');
+
+		mask >>= 1;
 	}
 }
+
+/*
+ * another method without creating a lengthy mask (100...)
+ * this while method starts at left beginning of binary and uses mask 1
+ *
+ * while (len >= 0)
+ * {
+ *    if ((n >> len) & 1)
+ *        _putchar('1');
+ *    else
+ *        _putchar('0');
+ *
+ *    len--;
+ * }
+ */
