48e66800d2883f7150c00704c56aeb971a5bf737
zamyalp <zamyalp03@gmail.com>
2022-07-12 18:14:00 -0700

    4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,17 +1,21 @@
 #include "lists.h"
+
 /**
- * free_listint - Entry Point
- * @head: head
- * Return: 0
+ * free_listint - frees a linked list
+ * @head: linked list
  */
+
 void free_listint(listint_t *head)
 {
-	if (head == NULL)
+	listint_t *ptr;
+
+	if (head == NULL) /* account for no linked list */
 		return;
 
-	while (head)
+	while (head != NULL) /* have ptr keep track of head node and free */
 	{
-		free(head);
-		head = head->next;
+		ptr = head;
+		head = head->next; /* move to next node while ptr frees prior */
+		free(ptr);
 	}
 }
