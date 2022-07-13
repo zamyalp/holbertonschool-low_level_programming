359dd4058b6b8862e128c9745d2085bd5d5d8df2
zamyalp <zamyalp03@gmail.com>
2022-07-13 16:40:50 -0700

    4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * clear_bit - Entry Point
+ * @n: input
+ * @index: index
+ * Return: 0
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int max_bits;
+	unsigned long int mask = 1;
+
+	/* validate index is not out of range */
+	max_bits = (sizeof(unsigned long int) * 8);
+	if (index > max_bits)
+		return (-1);
+
+	/* create mask with 0 at index (...11011...) to work on that index */
+	mask = ~(mask << index);
+	*n = (*n & mask);
+
+	return (1);
+}
